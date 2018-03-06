324abfe8b2beff26e113d645adb8e7e694f54aca
Borkes <1043568089@qq.com>
2018-03-06 20:45:56 +0800

    修改名字

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const simple_csv = require('./lib');
+const file_csv = require('./lib');
 
 module.exports = function (data, path) { 
-   return new simple_csv(data, path)
+   return new file_csv(data, path)
 }
